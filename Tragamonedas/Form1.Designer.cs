df44dd4ce189f3115dc10ab1ed5d725edec9bab2
Steven Martinez <esteven123123@outlook.com>
2022-04-28 23:27:00 -0400

    Add project files.

---
+++ Tragamonedas/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿
+namespace Tragamonedas
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Transparent;
+            this.panel2.Location = new System.Drawing.Point(295, 276);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(120, 97);
+            this.panel2.TabIndex = 1;
+            this.panel2.Tag = "icon";
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Transparent;
+            this.panel3.Location = new System.Drawing.Point(436, 276);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(121, 97);
+            this.panel3.TabIndex = 2;
+            this.panel3.Tag = "icon";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(161, 407);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(99, 94);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(307, 407);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(99, 94);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(447, 407);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(99, 94);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(586, 407);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(99, 94);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 8;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(730, 407);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(99, 94);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 9;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox6.Image = global::Tragamonedas.Properties.Resources.roulette_PNG18;
+            this.pictureBox6.Location = new System.Drawing.Point(324, 12);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(348, 222);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 10;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Transparent;
+            this.panel4.Location = new System.Drawing.Point(579, 276);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(119, 97);
+            this.panel4.TabIndex = 3;
+            this.panel4.Tag = "icon";
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Transparent;
+            this.panel5.Location = new System.Drawing.Point(717, 276);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(125, 97);
+            this.panel5.TabIndex = 4;
+            this.panel5.Tag = "icon";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Location = new System.Drawing.Point(149, 276);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(121, 97);
+            this.panel1.TabIndex = 2;
+            this.panel1.Tag = "icon";
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox7.Image = global::Tragamonedas.Properties.Resources._81F09NIVOGL;
+            this.pictureBox7.Location = new System.Drawing.Point(436, 523);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(121, 90);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 11;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox7_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Interval = 1;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Enabled = true;
+            this.timer4.Interval = 1;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // timer5
+            // 
+            this.timer5.Enabled = true;
+            this.timer5.Interval = 1;
+            this.timer5.Tick += new System.EventHandler(this.timer5_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(994, 625);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Slot Machine";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer5;
+    }
+}
+
---
+++ Tragamonedas/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Media;
+using System.Windows.Forms;
+using Tragamonedas.Properties;
+
+namespace Tragamonedas
+{
+    public partial class Form1 : Form
+    {
+        private readonly List<PictureBox> GrupoDeFichasUno = new();
+
+        private readonly List<PictureBox> GrupoDeFichasDos = new();
+
+        private readonly List<PictureBox> GrupoDeFichasTres = new();
+
+        private readonly List<PictureBox> GrupoDeFichasCuatro = new();
+
+        private readonly List<PictureBox> GrupoDeFichasCinco = new();
+
+        private int MovimientoY, PosicionSiguiente;
+        private readonly int posicionEjeX = 3;
+        private bool indicador1;
+        private bool indicador2;
+        private bool indicador3;
+        private bool indicador4;
+        private bool indicador5;
+        private string nombreFicha1 = "";
+        private string nombreFicha2 = "";
+        private string nombreFicha3 = "";
+        private string nombreFicha4 = "";
+        private string nombreFicha5 = "";
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        public List<PictureBox> CrearImagenes(List<PictureBox> Lista, Panel panel, string NombreTag)
+        {
+            var random = new Random();
+            var LongitudFicha = 100;
+            for (var i = 0; i < 20; i++)
+            {
+                var pb = new PictureBox();
+                pb.Image = (Bitmap) Resources.ResourceManager.GetObject("icons" + random.Next(1,8), Resources.Culture);
+                pb.Size = new Size(LongitudFicha, LongitudFicha);
+                pb.SizeMode = PictureBoxSizeMode.StretchImage;
+                pb.Name = string.Format("{0}", i + NombreTag);
+                pb.Tag = NombreTag + "_" + i;
+                var Mov = pb.Location.Y;
+                pb.Location = new Point(posicionEjeX, i * LongitudFicha);
+                Lista.Add(pb);
+                panel.Controls.Add(Lista[i]);
+            }
+
+            return Lista;
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Stop();
+            timer2.Stop();
+            timer3.Stop();
+            timer4.Stop();
+            timer5.Stop();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            nombreFicha2 = GirarFichas(GrupoDeFichasDos, panel2, timer2, indicador2);
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            nombreFicha3 = GirarFichas(GrupoDeFichasTres, panel3, timer3, indicador3);
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            nombreFicha4 = GirarFichas(GrupoDeFichasCuatro, panel4, timer4, indicador4);
+        }
+
+        private void timer5_Tick(object sender, EventArgs e)
+        {
+            nombreFicha5 = GirarFichas(GrupoDeFichasCinco, panel5, timer5, indicador5);
+
+            if (nombreFicha5 != "")
+            {
+                if (NumeroFicha(nombreFicha1) == NumeroFicha(nombreFicha2) &&
+                    NumeroFicha(nombreFicha2) == NumeroFicha(nombreFicha3) &&
+                    NumeroFicha(nombreFicha3) == NumeroFicha(nombreFicha4) &&
+                    NumeroFicha(nombreFicha4) == NumeroFicha(nombreFicha5))
+                {
+                    var form2 = new Form2();
+                    form2.Show();
+                    this.Visible = false;
+                }
+                else
+                {
+                    var form3 = new GameOver();
+                    form3.Show();
+                    this.Visible = false;
+                }
+            }
+            
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            
+            if (timer2.Enabled)
+            {
+                indicador2 = true;
+                timer3.Start();
+                pictureBox2.Image = Resources._1200px_Button_Icon_Green_svg;
+            }
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            
+            if (timer3.Enabled)
+            {
+                indicador3 = true;
+                timer4.Start();
+                pictureBox3.Image = Resources._1200px_Button_Icon_Green_svg;
+            }
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            
+            if (timer4.Enabled)
+            {
+                indicador4 = true;
+                timer5.Start();
+                pictureBox4.Image = Resources._1200px_Button_Icon_Green_svg;
+            }
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            
+            if (timer5.Enabled)
+            {
+                indicador5 = true;
+                pictureBox5.Image = Resources._1200px_Button_Icon_Green_svg;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+            if (timer1.Enabled)
+            {
+                pictureBox7.Enabled = false;
+                indicador1 = true;
+                timer2.Start();
+                pictureBox1.Image = Resources._1200px_Button_Icon_Green_svg;
+                
+            }
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+            indicador1 = false;
+            indicador2 = false;
+            indicador3 = false;
+            indicador4 = false;
+            indicador5 = false;
+            timer1.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ResetImages();
+        }
+
+        public void ResetImages()
+        {
+            CrearImagenes(GrupoDeFichasUno, panel1, "Uno");
+            CrearImagenes(GrupoDeFichasDos, panel2, "Dos");
+            CrearImagenes(GrupoDeFichasTres, panel3, "Tres");
+            CrearImagenes(GrupoDeFichasCuatro, panel4, "Cuatro");
+            CrearImagenes(GrupoDeFichasCinco, panel5, "Cinco");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            nombreFicha1 = GirarFichas(GrupoDeFichasUno, panel1, timer1, indicador1);
+        }
+
+
+        public string GirarFichas(List<PictureBox> Lista, Panel panel, Timer timer, bool bandera)
+        {
+            var FichaSeleccionada = "";
+            for (var i = 0; i < Lista.Count; i++)
+            {
+                MovimientoY = Lista[i].Location.Y;
+                Lista[i].Location = new Point(posicionEjeX, MovimientoY - 10);
+                if (Lista[i].Location.Y <= -100)
+                {
+                    PosicionSiguiente = Lista[Lista.Count - 1].Location.Y;
+                    Lista[i].Location = new Point(posicionEjeX, PosicionSiguiente + 100);
+                    Lista.Add(Lista[i]);
+                    panel.Controls.Add(Lista[i]);
+                    Lista.RemoveAt(i);
+                }
+
+                if (bandera)
+                {
+                    if (Lista[i].Location.Y == -10)
+                    {
+                        timer.Stop();
+                        FichaSeleccionada = Lista[i].Tag.ToString();
+                    }
+                }
+                else
+                {
+                    FichaSeleccionada = "";
+                }
+            }
+
+            return FichaSeleccionada;
+        }
+
+        public int NumeroFicha(string Nombre)
+        {
+            var Posicion = Nombre.Split("_".ToCharArray());
+            return Convert.ToInt32(Posicion[1]);
+        }
+
+        
+
+    }
+}
